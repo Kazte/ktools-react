7aff0a7aa2b3a83bfbc25cebbcc8e4bdd3e2f0d6
Kazte <39470734+Kazte@users.noreply.github.com>
2024-05-19 13:15:58 -0300

    chore: Update project configuration files

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import Case from '../case/case';
-import Default from '../default/default';
+import * as React from 'react'
 
 interface SwitchProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default function Switch({ children }: SwitchProps) {
-  let matchChild: React.ReactNode = null;
-  let defaultCase: React.ReactNode = null;
+  let matchChild: React.ReactNode = null
+  let defaultCase: React.ReactNode = null
 
   React.Children.forEach(children, (child: any) => {
     if (!matchChild && child.type == Case) {
-      const { condition } = child.props;
+      const { condition } = child.props
 
-      const conditionResult = Boolean(condition);
+      const conditionResult = Boolean(condition)
 
       if (conditionResult) {
-        matchChild = child;
+        matchChild = child
       }
     } else if (!defaultCase && child.type == Default) {
-      defaultCase = child;
+      defaultCase = child
     }
-  });
+  })
 
-  return matchChild ?? defaultCase ?? null;
+  return matchChild ?? defaultCase ?? null
 }
 
-Switch.Case = Case;
-Switch.Default = Default;
+Switch.Case = Case
+Switch.Default = Default
 
 interface DefaultProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 function Default({ children }: DefaultProps) {
-  return <>{children}</>;
+  return <>{children}</>
 }
 
 interface CaseProps {
-  condition: boolean;
-  children: React.ReactNode;
+  condition: boolean
+  children: React.ReactNode
 }
 
 function Case({ children, condition }: CaseProps) {
-  condition;
-  return <>{children}</>;
+  condition
+  return <>{children}</>
 }
